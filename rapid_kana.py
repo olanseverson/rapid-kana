bd2bc0185690e1a923123ccdeadab4831be5ac42
olanseverson <olanseverson13@gmail.com>
2020-03-20 15:52:43 +0900

    display letter only, dont display the 'particle'

--- rapid_kana.py
+++ rapid_kana.py
@@ -3,6 +3,7 @@ import time
 import os
 from random import randint
 
+
 # CONFIGURATION FILE
 DIRNAME, FILENAME = os.path.split(os.path.abspath(__file__))
 SOURCE_FILE = "news.txt"
@@ -18,9 +19,19 @@ class UserInterface:
         print(word)
 
     def display_answer(self, word):
-        print(word);
+        print(word)
+        
     def get_response(self,):
         return input()
+
+    def display_score(self, score, max_score):
+        print("You get "+str(score)+" from "+ str(max_score))
+
+    def display_speed(self, time, score):
+        print("SPEED: "+str(score/time)+" word/minute")
+
+    def display_time(self, time):
+        print("Total time: "+str(time)+' seconds')
     
 class Timer:
     def __init__(self,):
@@ -36,6 +47,9 @@ class Timer:
         return time.perf_counter()-self._start_time
 
 class RapidKana:
+    EXIT= 'q'
+    PARTICLE_LIST=['ga','ka','no','kara','made','de','ni','wo','he','to','’','nado']
+    NONLETTER = '~!,.1234567890;:"`"'
     _script = None
     _ui = None
     _num_of_correct_ans = 0
@@ -76,24 +90,46 @@ class RapidKana:
         time_elapse = self._timer.stop()
         self._ui.display(time_elapse)
 
+    def is_particle(self, word):
+        if wanakana.to_romaji(word) in self.PARTICLE_LIST:
+            return True
+        else:
+            return False
+
+    def only_letters(self, word):
+        for char in self.NONLETTER:
+            word = word.replace(char, "");
+        return word
+    
     def random_reading(self,):
         self._timer.start()
+        word_counter = 0
         for each_word in self._script:
+            word_counter=word_counter+1
             rand_num=randint(0,len(self._script)-1)
-            self._ui.display(self._script[rand_num])
+            word = self._script[rand_num];
+            if (self.is_particle(word)):
+                continue
+            self._ui.display(word)
             response = self._ui.get_response()
-            correct_answer=wanakana.to_romaji(self._script[rand_num])
+
+            if (response == self.EXIT):
+                break;
+            correct_answer=self.only_letters(wanakana.to_romaji(word))
             if (response == correct_answer):
                 self._num_of_correct_ans = self._num_of_correct_ans+1
             else :
                 self._ui.display_answer(correct_answer)
                 
-        time_elapse = self._timer.stop()
-        self._ui.display(time_elapse)
+        time_elapse = int(self._timer.stop())
+        self._ui.display_time(time_elapse)
+        self._ui.display_score(self._num_of_correct_ans, word_counter)
+        self._ui.display_speed(time_elapse, self._num_of_correct_ans)
 
 def main():
-    kana_reader=RapidKana('offline')
-    kana_reader.random_reading()
+    #kana_reader=RapidKana('offline')
+    #kana_reader.random_reading()
+    
     
 if __name__ == '__main__':
     main()
